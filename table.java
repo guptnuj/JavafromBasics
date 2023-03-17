72e5d95a7191c6373ba9babfe8f634a6f471ee47
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-17 21:03:35 +0530

    tables of given number

---
+++ table.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class table {
+
+    public static void main(String[] args) {
+        System.out.println("enter number:");
+        Scanner sc = new Scanner(System.in);
+        int tablenumber = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            int value = (tablenumber*i);
+            System.out.println(tablenumber + " * "+ i +"=" + value);
+        }
+    }
+}
