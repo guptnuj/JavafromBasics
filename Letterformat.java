afb5f58fc10a5b2e0f416c3ca638f26f3b797c1a
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-17 13:27:21 +0530

    String methods

---
+++ Letterformat.java
@@ -0,0 +1,7 @@
+public class Letterformat {
+    public static void main(String[] args){
+
+        String Letter = "Hello, \n\t I am Anuj ";
+                System.out.println(Letter);
+    }
+}
---
+++ Strin.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+
+public class Strin {
+    public static void main(String[] args){
+        String s = new String(" anuj Kumar ");
+        System.out.println(s);
+        System.out.println(s.trim());
+        System.out.println(s.length());
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.substring(1,3));
+        System.out.println(s.replace('n','j'));
+        s = "anu";
+        System.out.println(s);
+    }
+}
