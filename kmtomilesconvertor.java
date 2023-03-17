697a82c776377cbfca8aa1dbaa403a4a9cdb9025
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-17 13:26:31 +0530

    Readme

---
+++ kmtomilesconvertor.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class kmtomilesconvertor {
+
+
+
+        public static void main(String[] args){
+
+            System.out.println("Enter Distance in KMs");
+
+            Scanner sc = new Scanner(System.in);
+
+            float  distanceofkms = sc.nextFloat();
+
+            System.out.println(distanceofkms*0.62137119);
+
+        }
+}
