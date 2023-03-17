e61f4e4893418cd962e4562b0cb94fd86fa8f822
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-17 20:13:21 +0530

    Break statement

---
+++ BreakandCOntinue.java
@@ -0,0 +1,21 @@
+public class BreakandCOntinue {
+    public static void main(String[] args) {
+        for (int i =0;i<5;i++){
+            System.out.println("Enter");
+
+
+
+
+              if(i==2){
+                System.out.println("adsfnm");
+                  break;
+
+            }
+            System.out.println("i am king");
+
+        }
+
+
+
+    }
+}
