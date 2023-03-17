c665ff0a92447e81ea9c00b1e9bac66247df1ea8
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-17 14:10:56 +0530

    taking input from user in for loop

--- forloop.java
+++ forloop.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class forloop {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+
+        System.out.println("Enter number till you want to get numbers:");
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        for(int i=1;i <=b; i++) {
             System.out.println(i);
         }
     }
