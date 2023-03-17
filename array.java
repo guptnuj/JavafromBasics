1070c4d6438485a0d0ee9e07844e685a577d05d0
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-17 23:39:56 +0530

    Array intialize and accesing array elements using for loop

---
+++ array.java
@@ -0,0 +1,13 @@
+public class array {
+    public static void main(String[] args) {
+
+//accessing array using for loop
+
+        int [] a = new int[6]; //intializing array
+        a[0] =98;
+        System.out.println(a[1]);
+        for (int i =0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
