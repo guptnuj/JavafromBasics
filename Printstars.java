1ca261f614453aee55f9c11b5edf69bbeafea56a
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-17 20:36:01 +0530

    COntinue statement

---
+++ Printstars.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Printstars {
+    public static void main(String[] args) {
+        System.out.println("Enter number of Stars you want?");
+        Scanner sc = new Scanner(System.in);
+        int nooftimes = sc.nextInt();
+
+        for (int i=nooftimes;i>0;i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+
+        }
+
+        for (int i=0;i<nooftimes;i++) {
+            for (int j =0; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+
+        }
+    }
+}
