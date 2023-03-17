a3064b7749636ddf4d234dbc721c73278def7a8e
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-17 14:19:44 +0530

    Odd number

--- forloop.java
+++ forloop.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class forloop {
     public static void main(String[] args) {
 
-        System.out.println("Enter number till you want to get numbers:");
+        System.out.println("Enter number till you want to get odd numbers:");
         Scanner sc = new Scanner(System.in);
-        int b = sc.nextInt();
-        for(int i=1;i <=b; i++) {
-            System.out.println(i);
+        int number = sc.nextInt();
+        for(int i=0;i<number; i++) {
+
+           int  oddnumber = 2*i+1;
+            System.out.println(oddnumber);
         }
     }
 }
