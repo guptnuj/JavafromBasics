5b14babf39effa01de20dbae5ffcb20528e76f05
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-20 23:30:02 +0530

    Adding MD arrays

--- MDarray.java
+++ MDarray.java
@@ -1,7 +1,19 @@
 public class MDarray {
     public static void main(String[] args) {
-        int[][] a =new int[5][3];
-        a[0][0]= 2;
-        System.out.print(a.length);
+        int[][] a ={{1,2,3},
+                     {2,3,5}};
+
+            int[][] b = {{2,5,77},
+                        {7,99,0}};
+            int[][] result = new int[2][3];
+        for (int i =0;i< a.length;i++){
+            for (int c=0;c<a[i].length;c++){
+                result[i][c] = a[i][c] +b[i][c];
+                System.out.printf("Added element at %d and %d \n",i,c);
+                System.out.println(result[i][c]);
+            }
+        }
+
+
     }
 }
