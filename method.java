b2e57162d3ff01c095dc3a3e81fe423addc45a70
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-22 20:31:57 +0530

    Multiple methods with same name
    same name but different parameters

--- method.java
+++ method.java
@@ -4,18 +4,35 @@ import jdk.dynalink.beans.StaticClass;
 
 public class method {
 
-    //Creating method unstatic
+    //Creating method unstatic with same name but different parameters
     int um(int a, int b){
         int  c = a+b;
         System.out.println(c);
         return 2;
     }
+    int um(int a){
+
+        System.out.println(a);
+        return 1;
+    }
+    int um(int a, int b,int c){
+        int  z = a+b+c;
+        System.out.println(z);
+        return 3;
+    }
 
    public static void main(String[] args) {
     //calling method
 
-       method objjj =new method();
-       int   gumm =objjj.um(3,4);
+       method objjj  =new method();
+
+       int gumm =objjj.um(3,4);
+       int gum =objjj.um(2);
+       int gu = objjj.um(13,4,6);
        System.out.println(gumm);
+
+       //can create multiple methods with same name but parameters should be different
+
+
     }
 }
