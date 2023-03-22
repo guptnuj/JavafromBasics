006bb32a61ee676fcda6404ae06992b84a5f3544
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-23 01:57:41 +0530

    Practice

--- fibonacci.java
+++ fibonacci.java
@@ -1,23 +1,43 @@
 import java.util.Scanner;
 
 public class fibonacci {
+
+    static int fib(int a) {
+
+        if (a == 1 || a == 2) {
+            return a - 1;
+
+        } else {
+
+
+            int  k= fib(a-1) + fib(a-2);
+
+           return k;
+        }
+
+    }
+
     public static void main(String[] args) {
-//        ;
-        System.out.println("enter how many you want");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a =0;
-        int b =1;
-        System.out.print(a+" " +b+ " ");
-        for (int i=1;i<n;i++){
-           int  c =a+b;
-            System.out.print(c+ " ");
-            a =b;
-            b = c;
+
+
+int j = fib(11);
+
+        System.out.println(j);
+
+
+
+//        int a =0;
+//        int b =1;
+//        System.out.print(a+" " +b+ " ");
+//        for (int i=1;i<10;i++){
+//           int  c =a+b;
+//            System.out.print(c+ " ");
+//            a =b;
+//            b = c;
 
        }
 
 
 
     }
-}
+
