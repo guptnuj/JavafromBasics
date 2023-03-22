59de77415e15df1611a34abaf5610c31edecf1f4
Anuj Gupta <121455560+guptnuj@users.noreply.github.com>
2023-03-22 20:36:07 +0530

    Using return

---
+++ usingreturn.java
@@ -0,0 +1,9 @@
+public class usingreturn {
+    static int hu(int a, int b){
+      int  c = a+b;
+        return c;
+    }
+    public static void main(String[] args) {
+        System.out.println(hu(1,3));
+    }
+}
